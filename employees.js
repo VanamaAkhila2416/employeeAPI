40166eb6db514fd47eef843dd0ae10d4fa20190d
VanamaAkhila2416 <137475212+VanamaAkhila2416@users.noreply.github.com>
2024-02-16 15:48:31 +0530

    Add files via upload

---
+++ employees.js
@@ -0,0 +1,29 @@
+
+    const employees = [
+    {
+        id: 1,
+        name:'Ram',
+        email:'Ram@gmail.com',
+        department:'developemnt',
+        experience:'2yrs',
+        designation:'employee'
+    },
+    {
+        id: 2,
+        name:'Lakshmi',
+        email:'lakshmi@gmail.com',
+        department:'analyst',
+        experience:'3yrs',
+        designation:'Team Leader'
+    },
+    {
+        id: 3,
+        name:'Satish',
+        email:'Satish@gmail.com',
+        department:'security',
+        experience:'3yrs',
+        designation:'HR'
+    }   
+        
+];
+module.exports=employees;
